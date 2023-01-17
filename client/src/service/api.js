7c06a7b95323a40d97eeb196c0e923b978009488
Nandini Rastogi <81642738+Nandini-Rastogi@users.noreply.github.com>
2023-01-17 20:21:50 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getNews = async () =>{
-    const URL = '';
+    const URL = 'https://inshortsclone1.onrender.com';
     try{
         return await axios.get(`${URL}/news`);
     }catch(error){
